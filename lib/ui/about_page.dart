a5d9552dc26035b340b7c2ef80ed65f43e91cb2b
Mdominykas <marmadominykas@gmail.com>
2020-08-18 12:03:58 +0300

    changed about page

--- lib/ui/about_page.dart
+++ lib/ui/about_page.dart
@@ -19,9 +19,66 @@ class AboutPageBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Task 2
-    return Center(
-      child: Text("TODO: Task 2"),
+    return Column(
+      children: [
+        new Image.asset("assets/nma-logo.png"),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Flexible(
+                child: Container(
+                  child: Text(
+                      "Nacionalinė moksleivių akademija (NMA) – unikali papildomo ugdymo institucija, skirta mokslui ir muzikai gabiems Lietuvos vaikams. "
+                      "Nuo 2004 m. NMA ugdymo programas yra baigę daugiau nei 1300 talentingiausių vaikų iš įvairių Lietuvos kampelių. "
+                      "Akademijoje stiprinamas Lietuvos intelektinis potencialas, investuojama į gabius ir motyvuotus vaikus. "),
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Center(
+                      child: Text(
+                        "šiuo metu veikia 8 sekcijos",
+                        style: new TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Center(
+                      child: Text("matematika"),
+                    ),
+                    Center(
+                      child: Text("informatika"),
+                    ),
+                    Center(
+                      child: Text("biologija ir biochemija"),
+                    ),
+                    Center(
+                      child: Text("chemija"),
+                    ),
+                    Center(
+                      child: Text("fizika"),
+                    ),
+                    Center(
+                      child: Text("ekonomika"),
+                    ),
+                    Center(
+                      child: Text("lietuvių filologija"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        RaisedButton(
+          child: Text("puslapis"),
+          onPressed: _launchWebsite,
+        ),
+        RaisedButton(
+          child: Text("facebook"),
+          onPressed: _launchFacebook,
+        ),
+      ],
     );
   }
 
